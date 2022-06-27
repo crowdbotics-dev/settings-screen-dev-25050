5e337145a40f9410ce29fbf94e6f122ba358f613
Crowdbotics <engineering@crowdbotics.com>
2022-06-27 18:18:37 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,13 @@
+from django.conf import settings
 from django.db import models
+
+
+class Profile(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+    age = models.BigIntegerField()
+    location = models.CharField(
+        max_length=256,
+    )
